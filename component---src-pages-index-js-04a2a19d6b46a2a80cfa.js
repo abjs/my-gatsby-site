d5bf8b4d65fb63a8e8d5389d82c6e5a209dcc75b
Abin  Joseph <itsmeabjs@gmail.com>
2021-08-07 02:25:41 +0530

    Updates

---
+++ component---src-pages-index-js-04a2a19d6b46a2a80cfa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[678],{704:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var s=n(294);function a(){return s.createElement("div",{className:"main"},"hello world")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-04a2a19d6b46a2a80cfa.js.map
